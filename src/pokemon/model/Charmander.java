49022946653038539ea18744345a1e6b3b26ddae
kmart22 <kadenmartinsen@gmail.com>
2016-12-14 11:14:45 -0700

    added more interfaces and pokemon of the types

---
+++ src/pokemon/model/Charmander.java
@@ -0,0 +1,30 @@
+package pokemon.model;
+
+public class Charmander extends Pokemon implements Fire
+{
+	public Charmander()
+	{
+		super("Charmander", 4);
+		this.setAttackPoints(52);
+		this.setHealthPoints(39);
+		this.setSpeed(65.0);
+	}
+	
+	public Charmander(String name)
+	{
+		super("Charmander", 4);
+		this.setAttackPoints(52);
+		this.setHealthPoints(39);
+		this.setSpeed(65.0);
+	}
+	
+	public int fireBreath()
+	{
+		return 10;
+	}
+	
+	public int blazeKick()
+	{
+		return 20;
+	}
+}
---
+++ src/pokemon/model/Jigglypuff.java
@@ -0,0 +1,30 @@
+package pokemon.model;
+
+public class Jigglypuff extends Pokemon implements Fairy
+{
+	public Jigglypuff()
+	{
+		super("Jigglypuff", );
+		this.setAttackPoints();
+		this.setHealthPoints();
+		this.setSpeed();
+	}
+	
+	public Jigglypuff(String name)
+	{
+		super("Jigglypuff", );
+		this.setAttackPoints();
+		this.setHealthPoints();
+		this.setSpeed();
+	}
+	
+	public int charm()
+	{
+		return ;
+	}
+	
+	public int heal()
+	{
+		return ;
+	}
+}
